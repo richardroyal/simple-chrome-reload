63cec087c57fb2eb4ff7fa752e3a7b90f3c3d634
Richard Royal <richard@saidigital.co>
2012-10-22 11:20:11 -0400

    Re-activate originally activate window after reload click.

--- reload_open_tab.sh
+++ reload_open_tab.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
+
 # Click browser reload button and then return mouse to start location
+WINDOWID=$(xdotool getactivewindow)
+echo $WINDOWID
 eval $(xdotool getmouselocation --shell)
 xdotool mousemove 80 70 click 1
 xdotool mousemove $X $Y
+xdotool windowactivate $WINDOWID
